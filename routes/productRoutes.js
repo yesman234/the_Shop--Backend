a1acef75de852eaa3dbb35c34fc634459c614af9
Marcellus Devon Jones <mdjonesonly310@gmail.com>
2021-03-07 22:16:14 -0800

    done

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,15 +1,35 @@
 import express from "express";
+import {} from "express-async-handler";
+import products from "../data/products";
 const router = express.Router();
-import { getProductById, getProducts } from "../controllers/productControllers.js";
+import Product from "../models/productModel";
 //products
 // This will now be '/api/products' by default Hence name of file
 
 //@desc fetch all products
 //@route get req to /api/products
 //@access Public
-router.route('/').get(getProducts)
+router.get(
+	"/",
+	asyncHandler(async (req, res) => {
+		const products = await Product.find({});
+		res.json(products);
+	})
+);
 //@desc a single product
 //@route get req to /api/products
 //@access Public
-router.route('/:id').get(getProductById)
+router.get(
+	"/:id",
+	asyncHandler(async (req, res) => {
+		const product = await Product.findById(req.params.id);
+		if (product) {
+			res.json(product);
+		} else {
+			res.status(404);
+			throw new Error("Product not found");
+		}
+	})
+);
+
 export default router;
--- routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { authUser } from "../controllers/userControllers.js";
-
-router.post("/login", authUser);
-
-export default router;
